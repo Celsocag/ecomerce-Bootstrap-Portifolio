19b4c1b82d73eb5d1490b84c78fd7167439ddc42
Celsocag <celsoalbugo@gmail.com>
2020-09-13 16:50:38 -0300

    .

--- Public/js/main.js
+++ Public/js/main.js
@@ -15,7 +15,39 @@ $('.slider-two')
 .slick({
     prevArrow:".site-slider-two .prev",
     nextArrow:".site-slider-two .next",
+    autoplaySpeed: 3000,
+    dots: false,
     slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplaySpeed: 3000
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
 });
+
+// Responsive function
+
+$('.responsive').slick({
+    infinite: true,
+    
+  });
